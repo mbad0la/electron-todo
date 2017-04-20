618ae545783718d33242cee433d0a5cbddb28f59
Mayank Badola <badola21295@gmail.com>
2017-04-20 11:33:25 +0530

    Added title to notes | Minor ui adjustments for new note

--- src/components/App.js
+++ src/components/App.js
@@ -9,32 +9,45 @@ class NotesWrapper extends React.Component {
       buffer: '',
       notes: []
     }
-    this.newNoteBuffer = ''
+    this.newNoteContentBuffer = ''
+    this.newNoteTitleBuffer = ''
     this.addToNotes = this.addToNotes.bind(this)
-    this.updateBuffer = this.updateBuffer.bind(this)
+    this.updateContentBuffer = this.updateContentBuffer.bind(this)
+    this.updateTitleBuffer = this.updateTitleBuffer.bind(this)
   }
 
   addToNotes() {
     console.log(this.newNoteBuffer)
   }
 
-  updateBuffer(event) {
-    this.newNoteBuffer = event.target.innerHTML
+  updateContentBuffer(event) {
+    this.newNoteContentBuffer = event.target.innerHTML
+  }
+
+  updateTitleBuffer(event) {
+    this.newNoteTitleBuffer = event.target.innerHTML
   }
 
   render() {
     return (
-      <div className={styles.view}>
+      <div className={styles.newView}>
         <div className={styles.note}>
           <div className={styles.noteHead}>
             <div className={styles.new} onClick={this.addToNotes}>&#043;</div>
           </div>
           <div
+            className={styles.noteTitle}
+            contentEditable
+            suppressContentEditableWarning
+            placeholder="Heading"
+            onInput={this.updateTitleBuffer}>
+          </div>
+          <div
             className={styles.noteContent}
             contentEditable
             suppressContentEditableWarning
-            placeholder="Jot down ..."
-            onInput={this.updateBuffer}>
+            placeholder="Content"
+            onInput={this.updateContentBuffer}>
           </div>
           <div className={styles.noteFoot}>
           </div>
